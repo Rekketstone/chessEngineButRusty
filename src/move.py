24d8007b532a46641ad7d61d20aa7e8128b7cc7f
Rekketstone <113044698+Rekketstone@users.noreply.github.com>
2024-03-22 13:26:16 -0700

    Delete src directory

--- src/move.py
+++
@@ -1,7 +0,0 @@
-class Move:
-    def __init__(self, initital, final):  # initial and final are squares
-        self.initial = initital
-        self.final = final
-
-    def __eq__(self, other):
-        return self.initial == other.initial and self.final == other.final
